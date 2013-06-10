c6b848cecd3a635cf6d74fcc075980d42e99d488
Jad Jabbour <jabbourj.87@gmail.com>
2013-06-10 12:54:17 +0300

    changed _3 into function object in order to create instances of it thus managing multiple panels #jadscommits

--- _3.js
+++ _3.js
@@ -37,29 +37,27 @@
 /*
 
 My Notes:
-
-1- eventually _3 will be changed into a function_constructor for the object so this will allow multiple
-instances of _3 which means, multiple panels working independently.
-2- better error reporting
+1- Must write functions to GET Js/Css files for PAGE object accordingly.
+2- adjust file structure accordingly (in example) to test new model.
+3- make sure parent object is being parsed in all browsers/platforms.
+4- make sure all dataRepo management is being handled correctly
+5- need EXTENSIVE testing.
 
 */
-var _3 = {
-	Page : {
-		source : '', html : '', javascript : '', json : '', container : '', controls : [], dataRepo : '',
+var _3 = function (_source, loads){
+	this.Page = {
+		__3: this.parent, source : '', html : '', javascript : '', json : '', container : '', controls : [], dataRepo : '',
 		configurePage : function (_source){
-			this.source = _3.Helper.IsNullOrEmpty(_source.source) ? '' : _source.source;
-			this.html = _3.Helper.IsNullOrEmpty(_source.html) ? '' : _source.html;
-			this.javascript = _3.Helper.IsNullOrEmpty(_source.javascript) ? '' : _source.javascript;
-			this.json = _3.Helper.IsNullOrEmpty(_source.json) ? '' : _source.json;
-			this.container = _3.Helper.IsNullOrEmpty(_source.container) ? '' : _source.container;
-			this.dataRepo =  _3.Helper.IsNullOrEmpty(_source.dataRepo) ? '' : _source.dataRepo;
+			this.source = this.__3.Helper.IsNullOrEmpty(_source.source) ? '' : _source.source;
+			this.container = this.__3.Helper.IsNullOrEmpty(_source.container) ? '' : _source.container;
+			this.dataRepo =  this.__3.Helper.IsNullOrEmpty(_source.dataRepo) ? this.createDateRepo() : _source.dataRepo;
 			return this;
-		}, 
+		},
 		loadFront : function (reload_flag, with_pop){
-			if(_3.Helper.IsNullOrEmpty(this.html) || (_3.Helper.IsBoolean(reload_flag) && reload_flag)){
-				_3.RequestLoader.get('html', function (_pop){ 
+			if(this.__3.Helper.IsNullOrEmpty(this.html) || (this.__3.Helper.IsBoolean(reload_flag) && reload_flag)){
+				this.__3.RequestLoader.get('html', function (_pop){ 
 					if(_pop){
-						_3.Page.pop();
+						this.__3.Page.pop();
 					}
 				}, with_pop, null);
 			}
@@ -71,10 +69,10 @@ var _3 = {
 			return this;
 		},	
 		loadData : function (reload_flag, with_pop){
-			if(_3.Helper.IsNullOrEmpty(this.html) || (_3.Helper.IsBoolean(reload_flag) && reload_flag)){
-				_3.RequestLoader.get('json', function (_pop){ 
+			if(this.__3.Helper.IsNullOrEmpty(this.html) || (this.__3.Helper.IsBoolean(reload_flag) && reload_flag)){
+				this.__3.RequestLoader.get('json', function (_pop){ 
 					if(_pop){
-						_3.Page.pop();
+						this.__3.Page.pop();
 					}
 				}, with_pop, null);
 			}
@@ -86,10 +84,10 @@ var _3 = {
 			return this;
 		},
 		loadFunctionality : function (reload_flag, with_pop){
-			if(_3.Helper.IsNullOrEmpty(this.html) || (_3.Helper.IsBoolean(reload_flag) && reload_flag)){
-				_3.RequestLoader.get('javascript', function (_pop){ 
+			if(this.__3.Helper.IsNullOrEmpty(this.html) || (this.__3.Helper.IsBoolean(reload_flag) && reload_flag)){
+				this.__3.RequestLoader.get('javascript', function (_pop){ 
 					if(_pop){
-						_3.Page.pop();
+						this.__3.Page.pop();
 					}
 				}, with_pop, null);
 			}
@@ -112,103 +110,112 @@ var _3 = {
 			this.loadFunctionality(true, true);
 			return this;
 		},
-		serializePage : function (_current){
-			return _3.Helper.IsNullOrEmpty(_current) ? _3.Parser.serializeObject({ source : this.source, html : this.html, javascript : this.javascript, json : this.json, container : this.container }) : _3.Parser.serializeObject({ source : _current.source, html : _current.html, javascript : _current.javascript, json : _current.json, container : _current.container });
+		serializePage : function (_page){
+			return this.__3.Parser.serializeObject({ source : this.source, html : this.html, javascript : this.javascript, json : this.json, container : this.container });
 		},
 		pop : function (){
-			if(!_3.Helper.IsNullOrEmpty(this.html) && !_3.Helper.IsNullOrEmpty(this.javascript) && !_3.Helper.IsNullOrEmpty(this.json)){
-				_3.Helper.el(this.container).innerHTML = _3.Parser.bindDataToScreen(this);
-				_3.Parser.bindScript(this);	
+			if(!this.__3.Helper.IsNullOrEmpty(this.html) && !this.__3.Helper.IsNullOrEmpty(this.javascript) && !this.__3.Helper.IsNullOrEmpty(this.json)){
+				this.__3.Helper.el(this.container).innerHTML = this.__3.Parser.bindDataToScreen(this);
+				this.__3.Parser.bindScript(this);	
 			}
 			return this;
 		},
 		post : function (postData, responseFormat, IsInPostResponseRepo){
-			var data = _3.RequestLoader.buildPostData(postData, responseFormat);
+			var data = this.__3.RequestLoader.buildPostData(postData, responseFormat);
 			var callback = null;
-			if(!_3.Helper.IsNullOrEmpty(data)){
+			if(!this.__3.Helper.IsNullOrEmpty(data)){
 				if(responseFormat == 'json'){
 					if(IsInPostResponseRepo){
 						callback = function (response){
-							_3.Inject.pushToRepo(response);
+							this.__3.Inject.pushToRepo(response);
 						}
 					}
 					else{
 						callback = function (response){
-							_3.Inject.data(response.responseText);
+							this.__3.Inject.data(response.responseText);
 						}
 					}
 				}
 				if(responseFormat == 'javascript'){
 					callback = function (response){
-						_3.Inject.script(response.responseText);
+						this.__3.Inject.script(response.responseText);
 					}
 				}
-				_3.RequestLoader.post('post', data, null, null, callback);
+				this.__3.RequestLoader.post('post', data, null, null, callback);
 				return this;
 			}
 		},
 		getDataRepo : function(){
-			return JSON.parse(_3.Helper.el(this.dataRepo).innerText);
+			return JSON.parse(this.__3.Helper.el(this.dataRepo).innerText);
 		},
 		addToDataRepo : function (data){
 			var timesign = "data_" + new Date().getTime().toString();
-			_3.Helper.el(this.dataRepo).innerText += "," + timesign + "={" + JSON.stringify(data) + "}";
+			this.__3.Helper.el(this.dataRepo).innerText += "," + timesign + "={" + JSON.stringify(data) + "}";
 			return timesign;
+		},
+		createDateRepo : function (){
+			var element = document.createElement('input');
+			element.type = 'hidden';
+			element.id = this.container + "__" + new Date().getTime().toString();
+			document.getElementsByTagName('body').appendChild(element);
+			return element.id;
 		}
-	},
-	Parser: {
+	};
+	this.Parser = {
+		__3: this.parent,
 		serializeObject : function (object){
-			var current = !_3.Helper.IsNullOrEmpty(object) ? object : [];
+			var current = !this.__3.Helper.IsNullOrEmpty(object) ? object : [];
 			var serializedString = '';
 			for (var key in current){
 				if (current.hasOwnProperty(key)){
-					if(_3.Helper.IsObject(current[key]) && !_3.Helper.IsNullOrEmpty(current[key])){
-						serializedString += _3.Parser.serializeObject(current[key]) + '&';
+					if(this.__3.Helper.IsObject(current[key]) && !this.__3.Helper.IsNullOrEmpty(current[key])){
+						serializedString += this.__3.Parser.serializeObject(current[key]) + '&';
 					}
-					else if(!_3.Helper.IsFunction(key)){
+					else if(!this.__3.Helper.IsFunction(key)){
 				    	serializedString += key + '=' + encodeURIComponent(current[key]) + '&';
 				    }
 				}
 			}
-			return _3.Helper.removeLastChar(serializedString);
+			return this.__3.Helper.removeLastChar(serializedString);
 		},
-		bindDataToScreen : function (page){
-			var data = JSON.parse(page.json);
-			var template = page.html;
-			this.popControls(page);
+		bindDataToScreen : function (){
+			var data = JSON.parse(this.__3.Page.json);
+			var template = this.__3.Page.html;
+			this.popControls(this.__3.Page);
 			for(var k in data){
 				template = template.replace(new RegExp('{' + k + '}','g'), data[k]);
 			}
 			return template;
 		},
-		bindScript : function (page){
-			return eval(page.javascript);
+		bindScript : function (){
+			return eval(this.__3.Page.javascript);
 		},
-		popControls : function (page){
+		popControls : function (){
 			var regX = new RegExp("id=[\"'][A-Z a-z 0-9 _-]*[\"']",'g');
-			page.controls.length = 0;
+			this.__3.Page.controls.length = 0;
 			while(1){
-				match = regX.exec(page.html);
+				match = regX.exec(this.__3.Page.html);
 				if(match == null){
 					break;
 				} 
-				page.controls.push(match[0].split('=')[1]);
+				this.__3.Page.controls.push(match[0].split('=')[1]);
 			}
-			return page;
+			return this.__3.Page;
 		}
-	},
-	Inject : {
+	};
+	this.Inject = {
+		__3: this.parent,
 		pushToRepo : function (data){
-			_3.Page.addToDataRepo(data);
+			this.__3.Page.addToDataRepo(data);
 		},
 		data : function (data){
-			var original = JSON.parse(_3.Page.json);
+			var original = JSON.parse(this.__3.Page.json);
 			data = JSON.parse(data);
 			for(var k in data){
 				original[k] = data[k];
 			}
-			_3.Page.json = JSON.stringify(original);
-			_3.Page.pop();
+			this.__3.Page.json = JSON.stringify(original);
+			this.__3.Page.pop();
 		},
 		script : function (script){
 			return eval(script);
@@ -228,8 +235,9 @@ var _3 = {
 			script.setAttribute("src", jsUrl);
 			head.appendChild(script);
 		} 
-	},
-	Helper : {
+	};
+	this.Helper = {
+		__3: this.parent,
 		el : function (id){
 			return document.getElementById(id);
 		},
@@ -282,11 +290,13 @@ var _3 = {
 					return eval(callback + '(' + paramaterObject + ')');
 				}
 			}
-			_3.ErrorSilo.addError({errorMessage : 'Invalid callback or parameter object.', timestamp : new Date().getTime()}, false);
+			this.__3.ErrorSilo.addError({errorMessage : 'Invalid callback or parameter object.', timestamp : new Date().getTime()}, false);
 			return null;
 		}
-	},
-	Notifier : {
+	};
+	this.Notifier = {
+		__3: this.parent,
+		notifyCallback : '',
 		notify : function (mode, message){
 			switch(mode){
 				case 'error':
@@ -309,18 +319,19 @@ var _3 = {
 		inform : function (message){
 			alert(message);
 		}
-	},
-	ErrorSilo : {
+	};
+	this.ErrorSilo = {
+		__3: this.parent,
 		errorsCount : 0,
 		errors : [],
 		debugMode : false,
 		addError : function (errorObject, notify){
-			if(!_3.Helper.IsNullOrEmpty(errorObject) && _3.Helper.trimString(errorObject) != ''){
+			if(!this.__3.Helper.IsNullOrEmpty(errorObject) && this.__3.Helper.trimString(errorObject) != ''){
 				this.errors.push(errorObject);
 				this.errorsCount++;
 			}
 			if(notify || this.debugMode){
-				_3.Notifier.notify('error', errorObject.errorMessage);
+				this.__3.Notifier.notify('error', errorObject.errorMessage);
 			}
 		},
 		clearErrors : function(){
@@ -328,9 +339,9 @@ var _3 = {
 			this.errorsCount = 0;
 		},
 		printAllErrors : function (printCallback){
-			if(!_3.Helper.IsNullOrEmpty(printCallback)){
+			if(!this.__3.Helper.IsNullOrEmpty(printCallback)){
 				for (var i = this.errors.length - 1; i >= 0; i--){
-					_3.Helper.execCallback(printCallback, this.errors[i]);
+					this.__3.Helper.execCallback(printCallback, this.errors[i]);
 				}
 			}
 			else{
@@ -338,52 +349,54 @@ var _3 = {
 			}
 		},
 		toggleDebug : function (state){
-			if(!_3.Helper.IsBoolean(state)){
+			if(!this.__3.Helper.IsBoolean(state)){
 				this.debugMode = state;
 			}
 			else{
 				this.debugMode = !this.debugMode;
 			}
 		}
-	},
-	RequestLoader : {
+	};
+	this.RequestLoader = {
+		__3: this.parent,
 		get : function (aspect, callback, parameters, onHandle){
-			var currUrl = _3.urlObject();
-			var callUrl = _3.urlObject(_3.Page.source + '/' + aspect + '/');
-			if(!_3.Helper.IsNullOrEmpty(currUrl) && !_3.Helper.IsNullOrEmpty(callUrl)){
-				return callUrl.hostname == currUrl.hostname ? new _3.XHR().get(callUrl.fullURL, callback, parameters, aspect, onHandle) : new _3.XDR().get(callUrl.fullURL, callback, parameters, aspect, onHandle);
+			var currUrl = this.__3.urlObject();
+			var callUrl = this.__3.urlObject(this.__3.Page.source + '/' + aspect + '/');
+			if(!this.__3.Helper.IsNullOrEmpty(currUrl) && !this.__3.Helper.IsNullOrEmpty(callUrl)){
+				return callUrl.hostname == currUrl.hostname ? new this.__3.XHR().get(callUrl.fullURL, callback, parameters, aspect, onHandle) : new this.__3.XDR().get(callUrl.fullURL, callback, parameters, aspect, onHandle);
 			}
 			else{
-				_3.ErrorSilo.addError({errorMessage : 'Current URL is not set.', timestamp : new Date().getTime()}, false);
+				this.__3.ErrorSilo.addError({errorMessage : 'Current URL is not set.', timestamp : new Date().getTime()}, false);
 				return null;
 			}
 		},
 		post : function (aspect, postData, callback, parameters, onHandle){
-			var currUrl = _3.urlObject();
-			var callUrl = _3.urlObject(_3.Page.source + '/post/');
-			if(!_3.Helper.IsNullOrEmpty(currUrl) && !_3.Helper.IsNullOrEmpty(callUrl)){
-				return callUrl.hostname == currUrl.hostname ? new _3.XHR().post(callUrl.fullURL, postData, callback, parameters, aspect, onHandle) : new _3.XDR().post(callUrl.fullURL, postData, callback, parameters, aspect, onHandle);
+			var currUrl = this.__3.urlObject();
+			var callUrl = this.__3.urlObject(this.__3.Page.source + '/post/');
+			if(!this.__3.Helper.IsNullOrEmpty(currUrl) && !this.__3.Helper.IsNullOrEmpty(callUrl)){
+				return callUrl.hostname == currUrl.hostname ? new this.__3.XHR().post(callUrl.fullURL, postData, callback, parameters, aspect, onHandle) : new this.__3.XDR().post(callUrl.fullURL, postData, callback, parameters, aspect, onHandle);
 			}
 			else{
-				_3.ErrorSilo.addError({errorMessage : 'Current URL is not set.', timestamp : new Date().getTime()}, false);
+				this.__3.ErrorSilo.addError({errorMessage : 'Current URL is not set.', timestamp : new Date().getTime()}, false);
 				return null;
 			}
 		},
 		buildPostData : function (data, responseFormat){
-			return _3.Parser.serializeObject({
+			return this.__3.Parser.serializeObject({
 				data : data,
 				format : responseFormat,
-				page : _3.Page.serializePage()
+				page : this.__3.Page.serializePage()
 			}).replace(new RegExp('%20', 'g'), '+');
 		}
-	},
-	XHR : function (){
+	};
+	this.XHR = function (){
+		this.__3 = this.parent;
 		this.xhr = null;
 		this.get = function (_url, callback, parameters, loadIn, onHandle){
 			this.xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 			this.xhr.onreadystatechange = function(){
-				_3.Handle(this, loadIn, onHandle);
-				_3.Helper.execCallback(callback, parameters);
+				this.__3.Handle(this, loadIn, onHandle);
+				this.__3.Helper.execCallback(callback, parameters);
 			}
 			this.xhr.open('GET', _url, true);
 			this.xhr.send();
@@ -392,8 +405,8 @@ var _3 = {
 		this.post = function (_url, postData, callback, parameters, loadIn, onHandle){
 			this.xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 			this.xhr.onreadystatechange = function(){
-				_3.Handle(this, loadIn, onHandle);
-				_3.Helper.execCallback(callback, parameters);
+				this.__3.Handle(this, loadIn, onHandle);
+				this.__3.Helper.execCallback(callback, parameters);
 			}
 			this.xhr.open('POST', _url, true);
 			this.xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -401,14 +414,15 @@ var _3 = {
 			return this;
 		};
 		return this;
-	},
-	XDR : function (){
+	};
+	this.XDR = function (){
+		this.__3 = this.parent;
 		this.xdr = null;
 		this.get = function (_url, callback, parameters, loadIn, onHandle){
 			this.xdr = new XDomainRequest();
 			this.xdr.onreadystatechange = function(){
-				_3.Handle(this, loadIn, onHandle);
-				_3.Helper.execCallback(callback, parameters);
+				this.__3.Handle(this, loadIn, onHandle);
+				this.__3.Helper.execCallback(callback, parameters);
 			}
 			this.xdr.open('GET', _url, true);
 			this.xdr.send();
@@ -417,8 +431,8 @@ var _3 = {
 		this.post = function (_url, postData, callback, parameters, loadIn, onHandle){
 			this.xdr = new XDomainRequest();
 			this.xdr.onreadystatechange = function(){
-				_3.Handle(this, loadIn, onHandle);
-				_3.Helper.execCallback(callback, parameters);
+				this.__3.Handle(this, loadIn, onHandle);
+				this.__3.Helper.execCallback(callback, parameters);
 			}
 			this.xdr.open('POST', _url, true);
 			this.xdr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -426,24 +440,26 @@ var _3 = {
 			return this;
 		};
 		return this;
-	},
-	Handle : function (requestObject, loadIn, callback){
+	};
+	this.Handle = function (requestObject, loadIn, callback){
+		this.__3 = this.parent;
 		if (requestObject.readyState == 4){
 			if(requestObject.status == 200){
 				if(loadIn != 'post'){
-					_3.Page[loadIn] = requestObject.responseText;
+					this.__3.Page[loadIn] = requestObject.responseText;
 				}
-				_3.Helper.execCallback(callback, requestObject);
+				this.__3.Helper.execCallback(callback, requestObject);
 	    	}
 	    	else{
-				_3.ErrorSilo.addError({errorMessage : 'x-HTTP request failed :: ' + requestObject.statusText, timestamp : new Date().getTime()}, false);
+				this.__3.ErrorSilo.addError({errorMessage : 'x-HTTP request failed :: ' + requestObject.statusText, timestamp : new Date().getTime()}, false);
 			}
 		}
-	},
-	urlObject : function(_url){
+	};
+	this.urlObject = function(_url){
+		this.__3 = this.parent;
     	var a,key,value,pair,params,variables;
 		a = document.createElement('a');
-		a.href = _3.Helper.IsNullOrEmpty(_url) ? window.location.href : _url;
+		a.href = this.__3.Helper.IsNullOrEmpty(_url) ? window.location.href : _url;
 		url_query = a.search.substring(1);
 		params = {};
 		variables = url_query.split('&');
@@ -481,5 +497,10 @@ var _3 = {
 			parameters:params
    		};
    		return urlObj;
-	}
+	};
+
+	this.Page.configurePage(_source);
+
+	if(loads)
+		this.Page.load(true, true);
 }
