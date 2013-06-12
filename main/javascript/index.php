d919fb07b827a1e818e933ca78e81b5fc08ccffd
Jad Jabbour <jabbourj.87@gmail.com>
2013-06-12 11:40:34 +0300

    updated/fixed object stack & implemented new file structure/route part2

--- main/javascript/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo "$(document).ready(function () { $('#btn_submitname').click( function () { post_test(); }); });";
-die;
-?>
--- main/json/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$resultsArray = array();
-$resultsArray['from'] = "_3";
-$resultsArray['action'] = "says";
-$resultsArray['greeting'] = "Hello";
-$resultsArray['to'] = "World";
-echo json_encode($resultsArray);
-die;
-?>
